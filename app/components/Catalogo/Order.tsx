eeec7f235c09f907bc94806b1e26e229d00c99d7
PSjoon <pedrosantos.joon@gmail.com>
2024-04-24 21:46:19 -0300

    feat: Adicionar ordenar por

--- app/components/Catalogo/Order.tsx
+++ app/components/Catalogo/Order.tsx
@@ -1,8 +1,14 @@
+import Arrow from "@/public/Arrow.svg"
+import Image from "next/image"
+
 export function Orders() {
   return (
     <>
       <nav className="flex flex-col ml-72 gap-6 my-8">
-        <div>Ordenar por</div>
+        <div className="flex gap-4">
+          <p>Ordenar por</p>
+          <Image src={Arrow} alt="" className="w-6 h-6" />
+        </div>
         <div className="flex gap-2 ">
           <p className="w-6 h-6 p-1 flex justify-center items-center bg-white-300 rounded-lg">
             1
