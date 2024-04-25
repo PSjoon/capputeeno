0904d758f9833c7dc6a56b23310218da58fe92a0
PSjoon <pedrosantos.joon@gmail.com>
2024-04-24 21:20:37 -0300

    feat: addicionar itens corpo

--- app/components/Catalogo/Corpo.tsx
+++ app/components/Catalogo/Corpo.tsx
@@ -34,22 +34,22 @@ export function Corpo() {
     <main className="grid grid-cols-4 mx-36 place-items-center">
       {items.map((items) => {
         return (
-          <article key={items.id}>
-            <section className="flex flex-col justify-center bg-white-100 m-4 rounded-b-[4px]">
+          <article key={items.id} className="">
+            <section className=" flex flex-col justify-center bg-white-100 m-4 rounded-b-[4px]">
               <div className="">
                 <Image
                   src={items.image_url}
                   alt=""
                   width={500}
                   height={500}
-                  className="rounded-t-lg"
+                  className="rounded-t-lg h-[265px] object-cover"
                 ></Image>
               </div>
 
-              <div className="">
+              <div className="ml-4 my-2">
                 <p>{items.name}</p>
-                <div className="border-b border-white-900"></div>
-                <p>{items.price}</p>
+                <div className="w-[90%] my-2 border-b border-white-900"></div>
+                <p className="font-semibold text-black">R$ {items.price}</p>
               </div>
             </section>
           </article>
